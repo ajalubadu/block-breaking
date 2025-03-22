19388c85a40559c92d3a72d5a76fb77293da221b
ajalubadu <lionxx364@gmail.com>
2025-03-22 01:27:09 +0100

    added controlling speed with e & q

--- scripts/bar.gd
+++ scripts/bar.gd
@@ -9,5 +9,14 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = Vector2(input_axis * speed, 0)
 	
-	if position + velocity > Vector2(103, -INF) and position + velocity < Vector2(217, INF):
-		position += velocity
+	#if position + velocity > Vector2(103, -INF) and position + velocity < Vector2(217, INF):
+		#position += velocity
+	
+	position = clamp(position + velocity, Vector2(103, position.y), Vector2(217, position.y))
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("speed up"):
+		speed += 0.5
+	elif event.is_action_pressed("slow down"):
+		speed -= 0.5
