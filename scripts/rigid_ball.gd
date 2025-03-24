916a02154ca053cfd19d92d1c00587ad423eb09b
ajalubadu <lionxx364@gmail.com>
2025-03-24 17:24:53 +0100

    fixed play controlling ball bounce

--- scripts/rigid_ball.gd
+++ scripts/rigid_ball.gd
@@ -4,27 +4,34 @@ class_name Ball
 @export var starting_velocity = 100
 
 # 0 is fully intense
-const ANGLE_INTENSITY = 10
+const ANGLE_INTENSITY = 50
 
-var just_collided := false
+var last_velocity: Vector2
+
+
+func _physics_process(delta: float) -> void:
+	last_velocity = linear_velocity
 
 
 func _ready() -> void:
-	linear_velocity = Vector2(0,1).normalized() * starting_velocity
+	linear_velocity = Vector2(-1,1).normalized() * starting_velocity
 
 
 func _on_body_entered(body: Node) -> void:
 	if body is Block:
 		body.hit()
-	elif body is Bar and linear_velocity.dot(Vector2.DOWN) >= 0:
+	
+	# this below doesnt work
+	
+	elif body is Bar and last_velocity.y >= 0:
 		print("ALKFJKLF")
 		var normal = ((body.position + Vector2(0, ANGLE_INTENSITY - 5.5)) - position).normalized()
 		print(normal)
-		var perpendicular_velocity = linear_velocity.dot(normal) * normal
-		var up_velocity = linear_velocity.dot(Vector2.UP) * Vector2.UP
+		var perpendicular_velocity = last_velocity.dot(normal) * normal
+		var up_velocity = last_velocity.dot(Vector2.UP) * Vector2.UP
 		print(linear_velocity)
 		print(perpendicular_velocity)
-		var parallel_velocity = linear_velocity - perpendicular_velocity
+		var parallel_velocity = last_velocity - perpendicular_velocity
 		
 		var out_velocity = (-perpendicular_velocity) + (parallel_velocity)
 		
